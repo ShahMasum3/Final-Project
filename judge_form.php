21e668badd5ed8a4fec112887d0135fdc0e0d287
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 02:21:10 -0400

    Update judge_form.php

--- judge_form.php
+++ judge_form.php
@@ -1,64 +1,28 @@
-[1:52 AM, 5/9/2025] .: <?php
-include 'connect.php';
+<?php
 session_start();
+include 'connect.php';
 
-$total = $_POST["c1"] + $_POST["c2"] + $_POST["c3"] + $_POST["c4"];
-$jid = $_SESSION["judge_id"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $c1 = $_POST["c1"];
+    $c2 = $_POST["c2"];
+    $c3 = $_POST["c3"];
+    $c4 = $_POST["c4"];
+    $total = $c1 + $c2 + $c3 + $c4;
 
-$group_no = $_POST["group_no"];
-$group_members = $_POST["group_members"];
-$project_title = $_POST["project_title"];
-$c1 = $_POST["c1"];
-$c2 = $_POST["c2"];
-$c3 = $_POST["c3"];
-$c4 = $_POST["c4"];
-$judge_name = $_POST["judge_name"];
-$comments = $_POST["comments"];
+    $jid = $_SESSION["judge_id"];
+    $group_no = $_POST["group_no"];
+    $group_members = $_POST["group_members"];
+    $project_title = $_POST["project_title"];
+    $judge_name = $_POST["judge_name"];
+    $comments = $_POST["comments"];
 
-$sql = "INSERT INTO scores 
-    (judge_id, group_no, group_members, project_title, c1, c2, c3, c4, total, judge_name, comments)
-    VALUES (
-        '$jid', '$group_no', '$group_members', '$project_title',
-        '$c1', '$c2', '$c3', '$c4', '$total', '$judge_name', '$comments'
-    )";
+    $sql = "INSERT INTO scores (judge_id, group_no, group_members, project_title, c1, c2, c3, c4, total, judge_name, comments)
+            VALUES ('$jid', '$group_no', '$group_members', '$project_title', '$c1', '$c2', '$c3', '$c4', '$total', '$judge_name', '$comments')";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Score submitted!";
-} else {
-    echo "Error: " . mysqli_error($conn);
+    if (mysqli_query($conn, $sql)) {
+        echo "Score submitted!";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 ?>
-[2:06 AM, 5/9/2025] .: <?php
-include 'connect.php';
-session_start();
-
-if (!isset($_SESSION["judge_id"])) {
-    header("Location: judge_login.php");
-    exit;
-}
-?>
-
-<a href="admin.html">Go to Admin Page</a>
-<a href="database.html">Go to Database Page</a>
-
-<h2>Computer Science Project</h2>
-<form method="post" action="submit_score.php">
-  Group Number: <input name="group_no"><br><br>
-  Group Members: <input name="group_members"><br><br>
-  Project Title: <input name="project_title"><br><br>
-
-  <table border="1">
-    <tr><th>Criteria</th><th>Developing (0-10)</th><th>Accomplished (10-15)</th></tr>
-    <tr>
-      <td>Articulate requirements</td>
-      <td><input type="radio" name="c1" value="10"></td>
-      <td><input type="radio" name="c1" value="15"></td>
-    </tr>
-    <!-- Repeat this structure for c2, c3, c4 -->
-  </table>
-
-  Judge Name: <input name="judge_name"><br><br>
-  Comments: <input name="comments"><br><br>
-
-  <input type="submit" value="Submit">
-</form>
