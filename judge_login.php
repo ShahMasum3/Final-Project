edd9d0ee2e20c0689f64dc98a64d6e70642589c4
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 04:09:55 -0400

    Update judge_login.php

--- judge_login.php
+++ judge_login.php
@@ -5,12 +5,13 @@ include 'connect.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST["username"];
     $pass = $_POST["password"];
-
+    
     $res = mysqli_query($conn, "SELECT * FROM judges WHERE username='$user'");
     $row = mysqli_fetch_assoc($res);
 
     if ($row && password_verify($pass, $row["password"])) {
         $_SESSION["judge_id"] = $row["id"];
+        $_SESSION["judge_name"] = $user;
         header("Location: judge_form.php");
         exit;
     } else {
@@ -18,9 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-<h2>Judge Login</h2>
-<form method="post">
+    <form method="post">
   Username: <input name="username"><br>
   Password: <input type="password" name="password"><br>
   <input type="submit" value="Login">
