8931b8a25780d04d1c3b4cab995cb3c060f7d6d1
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 02:41:20 -0400

    Update judge_form.php

--- judge_form.php
+++ judge_form.php
@@ -2,27 +2,58 @@
 session_start();
 include 'connect.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $c1 = $_POST["c1"];
-    $c2 = $_POST["c2"];
-    $c3 = $_POST["c3"];
-    $c4 = $_POST["c4"];
-    $total = $c1 + $c2 + $c3 + $c4;
-
-    $jid = $_SESSION["judge_id"];
-    $group_no = $_POST["group_no"];
-    $group_members = $_POST["group_members"];
-    $project_title = $_POST["project_title"];
-    $judge_name = $_POST["judge_name"];
-    $comments = $_POST["comments"];
-
-    $sql = "INSERT INTO scores (judge_id, group_no, group_members, project_title, c1, c2, c3, c4, total, judge_name, comments)
-            VALUES ('$jid', '$group_no', '$group_members', '$project_title', '$c1', '$c2', '$c3', '$c4', '$total', '$judge_name', '$comments')";
-
-    if (mysqli_query($conn, $sql)) {
-        echo "Score submitted!";
-    } else {
-        echo "Error: " . mysqli_error($conn);
-    }
+// Redirect to login if judge is not logged in
+if (!isset($_SESSION["judge_id"])) {
+    header("Location: judge_login.php");
+    exit;
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Judge Form</title>
+</head>
+<body>
+    <h2>Computer Science Project Scoring</h2>
+    <form method="post" action="submit_score.php">
+        Group Number: <input name="group_no" required><br><br>
+        Group Members: <input name="group_members" required><br><br>
+        Project Title: <input name="project_title" required><br><br>
+
+        <table border="1" cellpadding="10">
+            <tr>
+                <th>Criteria</th>
+                <th>Developing (0-10)</th>
+                <th>Accomplished (10-15)</th>
+            </tr>
+            <tr>
+                <td>Articulate requirements</td>
+                <td><input type="radio" name="c1" value="10" required></td>
+                <td><input type="radio" name="c1" value="15"></td>
+            </tr>
+            <tr>
+                <td>Design solution</td>
+                <td><input type="radio" name="c2" value="10" required></td>
+                <td><input type="radio" name="c2" value="15"></td>
+            </tr>
+            <tr>
+                <td>Write code</td>
+                <td><input type="radio" name="c3" value="10" required></td>
+                <td><input type="radio" name="c3" value="15"></td>
+            </tr>
+            <tr>
+                <td>Test and debug</td>
+                <td><input type="radio" name="c4" value="10" required></td>
+                <td><input type="radio" name="c4" value="15"></td>
+            </tr>
+        </table>
+
+        <br>
+        Judge Name: <input name="judge_name" required><br><br>
+        Comments: <input name="comments"><br><br>
+
+        <input type="submit" value="Submit Score">
+    </form>
+</body>
+</html>
