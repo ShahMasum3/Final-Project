2de76b736852af2780789ac7841deeffbe2f8da4
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 04:16:53 -0400

    Update judge_login.php

--- judge_login.php
+++ judge_login.php
@@ -5,13 +5,16 @@ include 'connect.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST["username"];
     $pass = $_POST["password"];
-    
-    $res = mysqli_query($conn, "SELECT * FROM judges WHERE username='$user'");
-    $row = mysqli_fetch_assoc($res);
+
+    $stmt = mysqli_prepare($conn, "SELECT * FROM judges WHERE username = ?");
+    mysqli_stmt_bind_param($stmt, "s", $user);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
 
     if ($row && password_verify($pass, $row["password"])) {
         $_SESSION["judge_id"] = $row["id"];
-        $_SESSION["judge_name"] = $user;
+        $_SESSION["judge_name"] = $row["username"];
         header("Location: judge_form.php");
         exit;
     } else {
@@ -19,7 +22,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-    <form method="post">
+<h2>Judge Login</h2>
+<form method="post">
   Username: <input name="username"><br>
   Password: <input type="password" name="password"><br>
   <input type="submit" value="Login">
