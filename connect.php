759778b108acb35f21caf14d1c039bd291d42dce
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 01:28:59 -0400

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+$host = "sql3.freesqldatabase.com";
+$username = "sql3777783";
+$password = "cXZz9yKAvx";
+$database = "sql3777783";
+$port = 3306;
+
+$conn = mysqli_connect($host, $username, $password, $database, $port);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully!";
+?>
+
+
+Added connect.php
