48e4ca40b0055fced98fac936ed4058fb6a0c59a
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 03:05:09 -0400

    Update judge_login.php

--- judge_login.php
+++ judge_login.php
@@ -1,15 +1,4 @@
- <?php
-$host = "sql3.freesqldatabase.com";
-$username = "sql3777783";
-$password = "cXZz9yKAvx";
-$database = "sql3777783";
-$port = 3306;
-
-$conn = mysqli_connect($host, $username, $password, $database, $port);
-
-// REMOVE THIS LINE:
-// echo "Connected successfully! Added connect.php";
- <?php
+<?php
 session_start();
 include 'connect.php';
 
