04d489c653d7bcb2dcf3fa591ae2876ad017f571
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 02:14:24 -0400

    Update submit_score.php

--- submit_score.php
+++ submit_score.php
@@ -1,30 +1,32 @@
 <?php
-include 'connect.php';
 session_start();
+include 'connect.php';
 
-$total = $_POST["c1"] + $_POST["c2"] + $_POST["c3"] + $_POST["c4"];
-$jid = $_SESSION["judge_id"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $total = $_POST["c1"] + $_POST["c2"] + $_POST["c3"] + $_POST["c4"];
+    $jid = $_SESSION["judge_id"];
 
-$group_no = $_POST["group_no"];
-$group_members = $_POST["group_members"];
-$project_title = $_POST["project_title"];
-$c1 = $_POST["c1"];
-$c2 = $_POST["c2"];
-$c3 = $_POST["c3"];
-$c4 = $_POST["c4"];
-$judge_name = $_POST["judge_name"];
-$comments = $_POST["comments"];
+    $group_no = $_POST["group_no"];
+    $group_members = $_POST["group_members"];
+    $project_title = $_POST["project_title"];
+    $c1 = $_POST["c1"];
+    $c2 = $_POST["c2"];
+    $c3 = $_POST["c3"];
+    $c4 = $_POST["c4"];
+    $judge_name = $_POST["judge_name"];
+    $comments = $_POST["comments"];
 
-$sql = "INSERT INTO scores 
-    (judge_id, group_no, group_members, project_title, c1, c2, c3, c4, total, judge_name, comments)
-    VALUES (
-        '$jid', '$group_no', '$group_members', '$project_title',
-        '$c1', '$c2', '$c3', '$c4', '$total', '$judge_name', '$comments'
-    )";
+    $sql = "INSERT INTO scores 
+        (judge_id, group_no, group_members, project_title, c1, c2, c3, c4, total, judge_name, comments)
+        VALUES (
+            '$jid', '$group_no', '$group_members', '$project_title',
+            '$c1', '$c2', '$c3', '$c4', '$total', '$judge_name', '$comments'
+        )";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Score submitted!";
-} else {
-    echo "Error: " . mysqli_error($conn);
+    if (mysqli_query($conn, $sql)) {
+        echo "Score submitted!";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 ?>
