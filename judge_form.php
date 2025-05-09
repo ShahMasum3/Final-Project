6146a45016ef169ff5926081db4d950408285d56
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 02:07:42 -0400

    Update judge_form.php

--- judge_form.php
+++ judge_form.php
@@ -1,16 +1,46 @@
-<?php
+[1:52 AM, 5/9/2025] .: <?php
 include 'connect.php';
-<a href="admin.html">Go to Admin Page</a>
-
-
-<a href="database.html">Go to Database Page</a>
-
 session_start();
-if (!isset($_SESSION["judge_id"])) {
-  header("Location: judge_login.php");
-  exit;
+
+$total = $_POST["c1"] + $_POST["c2"] + $_POST["c3"] + $_POST["c4"];
+$jid = $_SESSION["judge_id"];
+
+$group_no = $_POST["group_no"];
+$group_members = $_POST["group_members"];
+$project_title = $_POST["project_title"];
+$c1 = $_POST["c1"];
+$c2 = $_POST["c2"];
+$c3 = $_POST["c3"];
+$c4 = $_POST["c4"];
+$judge_name = $_POST["judge_name"];
+$comments = $_POST["comments"];
+
+$sql = "INSERT INTO scores 
+    (judge_id, group_no, group_members, project_title, c1, c2, c3, c4, total, judge_name, comments)
+    VALUES (
+        '$jid', '$group_no', '$group_members', '$project_title',
+        '$c1', '$c2', '$c3', '$c4', '$total', '$judge_name', '$comments'
+    )";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Score submitted!";
+} else {
+    echo "Error: " . mysqli_error($conn);
 }
 ?>
+[2:06 AM, 5/9/2025] .: <?php
+include 'connect.php';
+session_start();
+
+if (!isset($_SESSION["judge_id"])) {
+    header("Location: judge_login.php");
+    exit;
+}
+?>
+
+<a href="admin.html">Go to Admin Page</a>
+<a href="database.html">Go to Database Page</a>
+
 <h2>Computer Science Project</h2>
 <form method="post" action="submit_score.php">
   Group Number: <input name="group_no"><br><br>
@@ -24,24 +54,11 @@ if (!isset($_SESSION["judge_id"])) {
       <td><input type="radio" name="c1" value="10"></td>
       <td><input type="radio" name="c1" value="15"></td>
     </tr>
-    <tr>
-      <td>Choose appropriate tools</td>
-      <td><input type="radio" name="c2" value="10"></td>
-      <td><input type="radio" name="c2" value="15"></td>
-    </tr>
-    <tr>
-      <td>Oral presentation</td>
-      <td><input type="radio" name="c3" value="10"></td>
-      <td><input type="radio" name="c3" value="15"></td>
-    </tr>
-    <tr>
-      <td>Teamwork</td>
-      <td><input type="radio" name="c4" value="10"></td>
-      <td><input type="radio" name="c4" value="15"></td>
-    </tr>
-  </table><br>
+    <!-- Repeat this structure for c2, c3, c4 -->
+  </table>
 
-  Judge Name:<input name="judge_name" value="<?= $_SESSION['judge_name'] ?>"><br>
-  Comments:<br><textarea name="comments" rows="3" cols="40"></textarea><br>
+  Judge Name: <input name="judge_name"><br><br>
+  Comments: <input name="comments"><br><br>
+
   <input type="submit" value="Submit">
 </form>
