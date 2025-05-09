bb7ecea5912bed8730954dfbb8de1c87a84d7b3c
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 04:26:49 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,12 @@
 <?php
 $host = "sql3.freesqldatabase.com";
 $username = "sql3777783";
-$password = "cXZz9yKAvx";
+$password = "CxZZ9ykXAvx";
 $database = "sql3777783";
 $port = 3306;
 
 $conn = mysqli_connect($host, $username, $password, $database, $port);
+
+
+mysqli_select_db($conn, $database);
+?>
