bff9a43b91a46476205e3fe23eb7d9c13258580d
ShahMasum3 <masumibne903@gmail.com>
2025-05-09 02:47:48 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,13 +6,3 @@ $database = "sql3777783";
 $port = 3306;
 
 $conn = mysqli_connect($host, $username, $password, $database, $port);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully!";
-?>
-
-
-Added connect.php
